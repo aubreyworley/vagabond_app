5b32ccc0adadf4daaa1a4859b69fade2c2bf36d0
astp <anniepeng@Annies-MacBook-Pro.local>
2015-08-10 16:33:48 -0700

    added authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def authorize
+  	unless current_user
+  		redirect_to login_path
+  	end
+  end
 end
