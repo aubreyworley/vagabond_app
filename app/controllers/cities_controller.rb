25c3a751dc4c28d6de7b07bf29ab39febb859d99
GregOSF <gregory.onza@gmail.com>
2015-08-12 15:40:45 -0700

    Test auth

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,8 +1,8 @@
 class CitiesController < ApplicationController
 
   def show
-    @stories = City.find(params[:id]).stories
-    @city = City.find(params[:id])
+    @stories = City.friendly.find(params[:id]).stories
+    @city = City.friendly.find(params[:id])
     render :show
   end
 
