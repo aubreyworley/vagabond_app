871824cd494a51d31865f0a6eef44ffdeb9f586f
GregOSF <gregory.onza@gmail.com>
2015-08-11 18:43:30 -0700

    Add edit, update delete routes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,6 @@
 class StoriesController < ApplicationController
   
+
   # show all stories in the db
   def index
     @stories = Story.all
@@ -15,6 +16,7 @@ class StoriesController < ApplicationController
   # creates new story in db that belongs to current_user
   def create
     story = current_user.stories.create(story_params)
+    # @story.user = current_user
     redirect_to story_path(story)
   end
 
@@ -24,33 +26,51 @@ class StoriesController < ApplicationController
   end
 
   def edit
+    @user = current_user
     @story = Story.find(params[:id])
-    if current_user.stories.include? @stories
+    # if current_user.stories.include? @story
       render :edit
-    else 
-      redirect_to profile_path
-    end
+    # else 
+    #   session[:user_id] = nil
+    #   redirect_to profile_path
+    # end
   end
 
   def update
+    @user = current_user
     story = Story.find(params[:id])
-    if current_user.stories.include? story
+    # if current_user.stories.include? story
       story.update_attributes(story_params)
       redirect_to story_path(story)
-    else
-      redirect_to profile_path
-    end
+    # else
+    #   redirect_to profile_path
+    # end
   end
 
   def destroy
+    @user = current_user
     story = Story.find(params[:id])
-    if current_user.stories.include? story
-      story.delete_attributes(story_params)
-      redirect_to story_path(story)
-    else 
+    # if current_user.stories.include? story
+      story.destroy
+    #   redirect_to story_path(story)
+    # else 
       redirect_to profile_path
-    end
+    # end
   end
+
+  # def destroy
+  #   story = Story.find(params[:id])
+  #   if current_user.stories.include? story
+  #     story.destroy
+  #     redirect_to profile_path
+  #   else
+  #     # if user tries to destroy recipe that doesn't belong to them
+  #     # log them out
+  #     session[:user_id] = nil
+  #     redirect_to login_path
+  #   end
+  # end
+
 
   private
     def story_params
